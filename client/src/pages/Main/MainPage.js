47636c8343adcc0cfe408367208f2c732a916514
jakrol <kozak10555@gmail.com>
2025-05-27 21:04:00 +0200

    .

--- client/src/pages/Main/MainPage.js
+++ client/src/pages/Main/MainPage.js
@@ -5,6 +5,7 @@ import { Navigate, Route, useNavigate } from "react-router-dom";
 import MoviePreviewPage from "./MoviePreviewPage";
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import styles from './Main.css';
+import fallback from '../../Img/missing_img.png';
 
 function MainPage() {
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -102,17 +103,33 @@ const searchByQuery = async () => {
         <input type="text" name="search" value={movieTitle} onChange={(e) => setMovieTitle(e.target.value)} placeholder="Search for movie"/>
         <button onClick={(e) => {setSelectedGenre(e.target.value); setPageNumber(1); setSearchMode('query'); searchByQuery()}}>TEST</button>
 
-
-          <div className="container-fluid">
+        <div className="container-fluid">
+          <div className="row g-4 px-3">
             {movies.map(movie => (
-              <div key={movie.id} className="movieContainer" onClick={() => handleMovieClick(movie)}>
-                <p>{movie.title}</p>
-                <p>{movie.release_date}</p>
-                <p>{movie.vote_average}</p>
-                <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="movie image poster" className="moviePoster"></img>
+              <div
+                key={movie.id}
+                className="col-12 col-sm-6 col-md-4 col-lg-3"
+                onClick={() => handleMovieClick(movie)}
+              >
+                <div className="card" tabIndex="0">
+                  <div className="card_image">
+                    <img src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : fallback} alt={`${movie.title} poster`} />
+                  </div>
+                  <div className="card_content">
+                    <h2 className="card_title">{movie.title}</h2>
+                    <div className="card_text">
+                      <p><strong>Release Date:</strong> {movie.release_date}</p>
+                      <p><strong>Rating:</strong> {movie.vote_average} / 10</p>
+                      <p>{movie.overview ? movie.overview : "No description available."}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
+        </div>
+
+
           
 
           <div className="navigation">
