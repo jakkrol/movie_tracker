ded9c4b6ba1e44d165889fbf6a578dd47d5d3cad
Jakub Król <kozak10555@gmail.com>
2025-07-16 15:14:13 +0200

    .

--- server/config/db.js
+++ server/config/db.js
@@ -3,16 +3,25 @@ const { Pool } = pkg;
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const config = {};
 
-pool.on("connect", ()=>{
-    console.log("Connection pool establised with DB");
+if (process.env.DATABASE_URL) {
+  config.connectionString = process.env.DATABASE_URL;
+  config.ssl = {
+    rejectUnauthorized: false, // needed for Supabase or any SSL DB with self-signed cert
+  };
+} else {
+  config.user = process.env.DB_USER;
+  config.host = process.env.DB_HOST;
+  config.database = process.env.DB_NAME;
+  config.password = process.env.DB_PASSWORD;
+  config.port = process.env.DB_PORT;
+}
+
+const pool = new Pool(config);
+
+pool.on("connect", () => {
+  console.log("Connection pool established with DB");
 });
 
 module.exports = pool;
