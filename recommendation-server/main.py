f8c006141ee8a6a646fc53ed793f4f8c17949be1
jakrol <kozak10555@gmail.com>
2026-03-31 16:30:42 +0200

    t

--- recommendation-server/main.py
+++ recommendation-server/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import nltk
+from nltk.stem import PorterStemmer
 import os
 
 from nltk.corpus import stopwords
@@ -9,8 +10,23 @@ from nltk.tokenize import word_tokenize
 path = os.path.join(os.getcwd(), 'nltk_data')
 nltk.data.path.append(path)
 
+required = ['punkt_tab', 'stopwords', 'wordnet', 'words'] 
+for res in required:
+    nltk.download(res, download_dir=path)
+
 app = Flask(__name__)
 CORS(app)
+
+
+word = "This is a test sentence to check the stop words removal process."
+output = word_tokenize(word)
+stop_words = set(stopwords.words('polish'))
+words = set(nltk.corpus.words.words())
+
+print("Original sentence:", word)
+print("Tokenized sentence:", output)
+print("Stop words:", stop_words)
+print("All words:", words)
 
 @app.route("/")
 def home():
