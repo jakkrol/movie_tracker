31228da30b635f778feadddafe02a3f5870ff2a3
jakrol <kozak10555@gmail.com>
2026-04-29 12:57:37 +0200

    added overviews preprocessing

--- recommendation-server/fetch.py
+++ recommendation-server/fetch.py
@@ -5,26 +5,40 @@ from dotenv import load_dotenv
 load_dotenv()
 TMDB_API_KEY = os.getenv('TMDB_API_KEY')
 
+def fetch_movie(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return {"title": data['title'], "overview": data['overview']}
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+
+
 def fetch_data(movie_id):
-# Endpoint dla rekomendacji
     url = f"https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={TMDB_API_KEY}&language=en-US"
     
     response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
-        results = data.get('results', [])
-        if results:
-            print("\n--- PRZYKŁADOWY OBIEKT FILMU Z API ---")
+        results = data.get('results', [])   
 
-            print(json.dumps(results[0], indent=4, ensure_ascii=False))
+        # if results:
+        #     print("\n--- PRZYKŁADOWY OBIEKT FILMU Z API ---")
+            # print(json.dumps(results[0], indent=4, ensure_ascii=False))
             
-            print("\n--- LISTA TYTUŁÓW I ICH OPISÓW ---")
-            for i, movie in enumerate(results[:5], 1):
-                print(f"{i}. {movie['title']} (ID: {movie['id']})")
-                print(f"   Opis: {movie['overview'][:100]}...")
-                print("-" * 20)
-        else:
-            print("Brak rekomendacji dla tego filmu.")
+            # print("\n--- LISTA TYTUŁÓW I ICH OPISÓW ---")
+            # for i, movie in enumerate(results[:5], 1):
+            #     print(f"{i}. {movie['title']} (ID: {movie['id']})")
+            #     print(f"   Opis: {movie['overview'][:100]}...")
+            #     print("-" * 20)
+
+        return [{"title": movie['title'], "overview": movie['overview']} for movie in results]
     else:
-        print(f"Błąd API: {response.status_code}")
+        print(f"Error: {response.status_code}")
+        return []
--- recommendation-server/main.py
+++ recommendation-server/main.py
@@ -1,30 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 import pandas as pd
 import numpy as np
-
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
 import os
 import requests
 import json
 
 from fetch import fetch_data
+from fetch import fetch_movie
 
 
 path = os.path.join(os.getcwd(), 'nltk_data')
 nltk.data.path.append(path)
 
-required = ['punkt_tab', 'stopwords', 'wordnet', 'words'] 
-for res in required:
-    nltk.download(res, download_dir=path)
+if not os.path.exists(path):
+    os.makedirs(path)
+    required = ['punkt_tab', 'stopwords', 'wordnet', 'words'] 
+    for res in required:
+        nltk.download(res, download_dir=path)
 
 app = Flask(__name__)
 CORS(app)
@@ -42,7 +41,27 @@ CORS(app)
 
 
 
+def preprocess_text():
+    checked_movie = fetch_movie(550)
+    recommended_movies =fetch_data(550)
+    print("Cosine similarity:", res, "Cosine similarity A-B:", res2)
 
+    checked_movie['overview'] = word_tokenize(checked_movie['overview'])
+    filtered_movie = [
+        word.lower() for word in checked_movie['overview'] if word.lower() not in stopwords and word.isalpha()
+    ]
+    checked_movie['overview'] = filtered_movie
+
+    for item in recommended_movies:
+        item['overview'] = word_tokenize(item['overview'])
+
+        filtered_overview = [
+            word.lower() for word in item['overview'] if word.lower() not in stopwords and word.isalpha() 
+        ]
+
+        item['overview'] = filtered_overview
+
+    return checked_movie, recommended_movies
 
 X = [0,0,0]
 Y = [1,1,1]
@@ -52,13 +71,14 @@ B = [1,1,1]
 res = cosine_similarity([X], [Y])
 res2 = cosine_similarity([A], [B])
 
+stopwords = set(stopwords.words('english'))
 
 
 @app.route("/")
 def home():
-    fetch_data(550)
-    print("Cosine similarity:", res, "Cosine similarity A-B:", res2)
-    return "Testing site"
+    data = preprocess_text()
+    checked_movie, recommended_movies = data
+    return jsonify(recommended_movies)
 
 
 
